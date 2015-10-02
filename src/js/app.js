5469fd7238f76e96aa070384ae48085aee48a266
Alexander Wallin <office@alexanderwallin.com>
2015-10-02 19:39:45 +0200

    Basic THREE setup

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,5 @@
 import Room from './room/room';
 
 let room = new Room({
-  canvas: document.querySelector('#canvas')
+  container: document.querySelector('#container')
 });
--- src/js/room/room.js
+++ src/js/room/room.js
@@ -1,7 +1,33 @@
 
+import THREE from 'three';
+
 export default class {
-  constructor(canvas) {
-    this.canvas = canvas;
-    console.log('room', canvas);
+  constructor(opts) {
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    this.camera.position.z = 1000;
+
+    this.scene = new THREE.Scene();
+
+    let geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    let material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    this.mesh = new THREE.Mesh( geometry, material );
+    this.scene.add( this.mesh );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    opts.container.appendChild( this.renderer.domElement );
+  }
+
+  animate() {
+ 
+    // note: three.js includes requestAnimationFrame shim 
+    requestAnimationFrame( this.animate );
+
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+
+    this.renderer.render( this.scene, this.camera );
   }
 };
