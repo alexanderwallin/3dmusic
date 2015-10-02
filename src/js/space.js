e55d2a3bf622bd4ec562d2ac8074766064b14e10
Alexander Wallin <office@alexanderwallin.com>
2015-10-02 21:21:32 +0200

    Adds space (now: axis vectors) constants

---
+++ src/js/space.js
@@ -0,0 +1,8 @@
+
+// Vendor libs
+import THREE from 'three';
+
+// Axises
+export const axisX = new THREE.Vector3(1, 0, 0);
+export const axisY = new THREE.Vector3(0, 1, 0);
+export const axisZ = new THREE.Vector3(0, 0, 1);
