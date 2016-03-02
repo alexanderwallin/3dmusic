ef90766697722f9ec2bca2a83f3b9ea0ec4f8e58
Alexander Wallin <office@alexanderwallin.com>
2016-03-02 02:31:07 +0100

    Adds and shows an, at this point, random version number

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 
 import Room from './room';
+import packageJson from '../../package.json';
 
 let room = new Room({
   container: document.querySelector('#container')
 });
+
+document.querySelector('#version').innerHTML = packageJson.version;
