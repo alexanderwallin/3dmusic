98bb32806fdd57770ee27f09902802ad4abb7f3a
Alexander Wallin <office@alexanderwallin.com>
2016-10-12 12:25:42 +0100

    Comments out the compressor's reduction parameter

--- src/js/fx/compressor.js
+++ src/js/fx/compressor.js
@@ -13,9 +13,12 @@ export default class {
     compressor.threshold.value = options.threshold || -50;
     compressor.knee.value      = options.knee      || 40;
     compressor.ratio.value     = options.ratio     || 12;
-    compressor.reduction.value = options.reduction || -20;
     compressor.attack.value    = options.attack    || 0.002;
     compressor.release.value   = options.release   || 0.25;
+
+    // The behavior of reduction differs between browsers, and might
+    // be a float or an AudioParam. We'll have to leave it be for now.
+    // compressor.reduction.value = options.reduction || -20;
 
     this.compressor = compressor;
     this.input = this.output = this.compressor;
